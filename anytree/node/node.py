e0e65e8c4361734beac79423331fb9968906490c
bgriffen <brendan.f.griffen@gmail.com>
2019-04-19 18:06:33 -0700

    SCB version

--- anytree/node/node.py
+++ anytree/node/node.py
@@ -6,7 +6,7 @@ from .util import _repr
 
 class Node(NodeMixin, object):
 
-    def __init__(self, name, parent=None, children=None, **kwargs):
+    def __init__(self, gate, name, index, parent=None, children=None, **kwargs):
         u"""
         A simple tree node with a `name` and any `kwargs`.
 
@@ -62,10 +62,25 @@ class Node(NodeMixin, object):
                 └── Node('/root/sub1/sub1C/sub1Ca')
         """
         self.__dict__.update(kwargs)
+        self.gate = gate
         self.name = name
         self.parent = parent
+        self.index = set(index)
         if children:
             self.children = children
+
+        
+        # ADDED BY B. GRIFFEN (brendan.f.griffen@gmail.com) 20190320
+
+        if self.parent == None:
+            self.id = 0
+        else:
+            all_node_ids = [node.id for node in self.root.descendants if hasattr(node,'id')]
+            if len(all_node_ids) == 0:
+                self.id = 1
+            else:
+                self.id = max(all_node_ids) + 1
+        
 
     def __repr__(self):
         args = ["%r" % self.separator.join([""] + [str(node.name) for node in self.path])]
